555a513ca70fcc9e5dacbe1f4155c91f21afcee5
koray95 <koraydogan95@gmail.com>
2022-03-18 12:20:24 +0300

    Clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Clock from './Clock.js';
+import Saat from  './Saat.js';
 
 function App() {
+  const [goster, setGoster] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vakit Nakittir</h1>
+      <input type="checkbox" checked={goster} onChange={(e)=> setGoster(e.target.checked)}/> Göster{goster? <Clock/>:null} 
+      <Saat/>
+      <Saat/>
+      <Saat/>
+
     </div>
   );
 }
